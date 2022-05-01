241e4d34125d17bc7be915ffc97eb55981371c2e
Wilco Kleynhans <wilco.kleynhans@gmail.com>
2022-05-01 11:17:41 +0200

    End of Section 3

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -14,14 +14,17 @@ namespace API.Controllers
     public class UsersController : ControllerBase
     {
         private readonly DataContext _context;
+      
         public UsersController(DataContext context)
         {
             _context = context;
+       
         }
 
         [HttpGet]
         public async Task<ActionResult<IEnumerable<AppUser>>>GetUsers()
         {
+            
             return await _context.Users.ToListAsync();
         }
 
